e12665128e19b2e26280d073bea1fc703a786bdb
ducviencse <nguyenducneiv@gmail.com>
2017-12-18 18:19:17 +0700

    add hog_svm model update data_helpers.py for hog_svm update README.md

--- data_helpers.py
+++ data_helpers.py
@@ -31,6 +31,27 @@ def load_svhn(path):
     print(X_test.shape[0], 'test samples')
     return X_train, Y_train, X_test, Y_test
 
+def load_shvn_for_svm(path):
+     train_data = scipy.io.loadmat(path + '/train_32x32.mat')
+    test_data = scipy.io.loadmat(path + '/test_32x32.mat')
+    x_train, y_train = train_data['X'], train_data['y']
+    x_test, y_test = test_data['X'], test_data['y']
+
+    X_train = np.transpose(x_train, [3, 0, 1, 2])
+    X_test = np.transpose(x_test, [3, 0, 1, 2])
+
+    X_train = X_train.astype('float32')
+    X_test = X_test.astype('float32')
+    X_train /= 255
+    X_test /= 255
+    Y_train = y_train.reshape(-1, ) - 1
+    Y_test = y_test.reshape(-1, ) - 1
+    # convert class vectors to binary class matrices
+    print('X_train shape:', X_train.shape)
+    print(X_train.shape[0], 'train samples')
+    print(X_test.shape[0], 'test samples')
+    return X_train, Y_train, X_test, Y_test
+
 
 def batch_iter(data, batch_size, num_epochs, shuffle=True):
     """
