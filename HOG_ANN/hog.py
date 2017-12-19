fa157b5c0f71c3f3bf05ec9d5dc2e9d976b3d481
Trung Trịnh <trinhtrung96@gmail.com>
2017-12-19 09:52:48 +0700

    Optimize code

--- HOG_ANN/hog.py
+++ HOG_ANN/hog.py
@@ -5,8 +5,8 @@ from skimage.feature import hog
 
 def apply_hog(image):
     image = color.rgb2gray(image)
-    hog_image = hog(image, orientations=9, pixels_per_cell=(
-        4, 4), cells_per_block=(2, 2), transform_sqrt=True)
+    hog_image = hog(image, orientations=9, pixels_per_cell=(4, 4), cells_per_block=(2, 2), transform_sqrt=True,
+                    block_norm='L2')
     return hog_image
 
 
--- train_eval_hog_svm.py
+++ train_eval_hog_svm.py
@@ -12,7 +12,8 @@ import data_helpers
 
 def apply_hog(image):
     image = color.rgb2gray(image)
-    hog_image = hog(image, orientations=9, pixels_per_cell=(4, 4), cells_per_block=(2, 2), transform_sqrt=True)
+    hog_image = hog(image, orientations=9, pixels_per_cell=(4, 4), cells_per_block=(2, 2), transform_sqrt=True,
+                    block_norm='L2')
     return hog_image
 
 
@@ -25,13 +26,14 @@ if __name__ == '__main__':
     parser.add_argument(
         '--data_dir',
         type=str,
+        default='./data',
         help='Directory for storing input data')
     FLAGS, unparsed = parser.parse_known_args()
     print(FLAGS)
     x_train, y_train, x_test, y_test = data_helpers.load_shvn_for_svm(FLAGS.data_dir)
     x_hog_train = hog_images(x_train)
     x_hog_test = hog_images(x_test)
-    sgdc = SGDClassifier(max_iter=50)
+    sgdc = SGDClassifier(max_iter=50, loss='hinge')
     sgdc.fit(x_hog_train, y_train)
     print(classification_report(y_pred=sgdc.predict(x_hog_test), y_true=y_test))
     print(accuracy_score(sgdc.predict(x_hog_test), y_test))
